cc381190789e602e3249af8e5c3c0822f8e5ebf7
MohammadTorki <torkimohammad4@gmail.com>
2020-11-30 00:04:03 +0330

    update again!

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 10;
-document.querySelector('.score').textContent = 15;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 15;
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(docuement.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a number! ⛔';
+  }
 });
